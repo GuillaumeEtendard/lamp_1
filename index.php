f4c3f76ddb57940f7f8bd1596af95ad5439fbc0a
GuillaumeEtendard <guillaumetendard@gmail.com>
2016-01-12 11:49:33 +0100

    correction ex0

--- index.php
+++ index.php
@@ -1,14 +1,46 @@
 <?php
-?>
+session_start();
+if(empty($_SESSION['choice']) || isset($_POST['reset'])){
+    $choice  =  rand(0,100);
+    $_SESSION['score'] = 0;
+    $_SESSION['choice'] = $choice;
+}else{
+    $choice = $_SESSION['choice'];
+}
 
+$response = null;
+if( !isset($_POST['guess'])
+    || empty($_POST['guess'])){
+    $response = "Pas de nombre";
+}else{
+    $guess = $_POST['guess'];
+    $_SESSION['score']++;
+    if($guess > $choice) {
+        $response = "C'est moins";
+    }elseif($guess < $choice){
+        $response = "C'est plus";
+    }else{
+        $response = "C'est gagné";
+        unset($_SESSION['choice']);
+    }
+}
+?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Index</title>
+    <title>Des papiers dans un bol </title>
 </head>
 <body>
 
+<?php echo $response;?>
+Nombre de coup : <?php echo $_SESSION['score']; ?>
+<form method="POST">
+    <input type="text" name="guess" autofocus>
+    <input type="submit">
+    <input type="submit" name="reset" value="reset">
 </form>
+<em>(La réponse est <?php echo $choice?>)</em>
+
 </body>
 </html>
